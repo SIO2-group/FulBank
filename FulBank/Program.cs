2cb127e5e0d2b15859a768c1d6eb1bc7009e14e6
BONIFACE <contact@yohannboniface.dev>
2021-09-16 15:48:33 +0200

    :art: Merging with Rider solution & english

--- FulBank/Program.cs
+++ FulBank/Program.cs
@@ -4,12 +4,12 @@ using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace Fulbank
+namespace FulBank
 {
     static class Program
     {
         /// <summary>
-        /// Point d'entrée principal de l'application.
+        /// The main entry point for the application.
         /// </summary>
         [STAThread]
         static void Main()
